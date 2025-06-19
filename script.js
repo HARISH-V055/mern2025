dc19ebf3c35cb87ef7b08d5a543ce44278234abe
harish <harishv.23it@kongu.edu>
2025-06-19 09:05:28 +0530

    Your commit message

---
+++ script.js
@@ -0,0 +1,24 @@
+var a;
+a=10;
+var a =20;
+console.log(a); 
+function sum(){
+    var a = 30;
+    console.log(a);
+}
+sum();
+var a=() =>{
+    console.log("Arrow function");
+}
+a();
+var marks = [90,91,92,100,95];
+var[a,b,c,d,e] = marks;
+console.log(a);
+console.log(b);     
+console.log(c);
+console.log(d);
+console.log(e);
+ var odd = Array.filter(num => num%2 !== 0)
+console.log(odd);
+
+
