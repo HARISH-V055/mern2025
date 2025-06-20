78043a81ca004291fdb40b13b4c52827d26340bd
harish <harishv.23it@kongu.edu>
2025-06-20 09:02:25 +0530

    Your commit message

---
+++ react/src/Pages/Skills.jsx
@@ -0,0 +1,18 @@
+import {Component} from 'react';
+class Skills extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Skills</h1>
+        <ul>
+          <li>JavaScript</li>
+          <li>React</li>
+          <li>Node.js</li>
+          <li>CSS</li>
+          <li>HTML</li>
+        </ul>
+      </div>
+    );
+  }
+}
+export default Skills;
---
+++ react/src/components/Child.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Child = ({name,pno}) => {
+  return (
+    <div><h1>NAME:{name} </h1></div>
+  )
+}
+
+export default Child;
