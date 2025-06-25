6ea9c39b384d2c05faba45932ecae3aafd4d8d7e
harish <harishv.23it@kongu.edu>
2025-06-25 12:01:51 +0530

    Fix case and naming for usdel.js

--- backend/server.js
+++ backend/server.js
@@ -3,7 +3,7 @@ const dotenv = require('dotenv');
 const connectDB = require('./config/db');
 const cors = require('cors');
 const router = require('./Routers/egRouters');
-const todorouter = require('./routers/todoRouters'); 
+const todoRouter = require('./routers/todoRouters');
 dotenv.config();
 const app = express();
 const PORT=3000;
@@ -11,7 +11,7 @@ connectDB();
 app.use(express.json());
 app.use(cors());
 app.use(router);
-app.use('/todo', todorouter);
+app.use('/todo', todoRouter);
 app.listen(PORT, () => {
     console.log(`Server is a running on port http://localhost:${PORT}`);
 })
